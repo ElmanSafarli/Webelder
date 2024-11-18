22e28b93ab9d40017d1bc21bc732c2be0c71287e
Elman Seferli <itiselman@gmail.com>
2024-11-18 20:29:43 +0400

    update

---
+++ src/shared/ui/Buttons/ArrowButton.jsx
@@ -0,0 +1,130 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const ArrowButton = ({ text, link }) => {
+  return (
+    <StyledWrapper>
+      <Link to={link} target="_blank" rel="noopener noreferrer">
+        <button className="animated-button">
+          <svg
+            width="18"
+            height="16"
+            viewBox="0 0 18 16"
+            className="arr-2"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM17.7071 8.70711C18.0976 8.31658 18.0976 7.68342 17.7071 7.29289L11.3431 0.928932C10.9526 0.538408 10.3195 0.538408 9.92893 0.928932C9.53841 1.31946 9.53841 1.95262 9.92893 2.34315L15.5858 8L9.92893 13.6569C9.53841 14.0474 9.53841 14.6805 9.92893 15.0711C10.3195 15.4616 10.9526 15.4616 11.3431 15.0711L17.7071 8.70711ZM1 9H17V7H1V9Z" />
+          </svg>
+          <span className="text">{text}</span>
+          <span className="circle" />
+          <svg
+            width="18"
+            height="16"
+            viewBox="0 0 18 16"
+            className="arr-1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M1 7C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9V7ZM17.7071 8.70711C18.0976 8.31658 18.0976 7.68342 17.7071 7.29289L11.3431 0.928932C10.9526 0.538408 10.3195 0.538408 9.92893 0.928932C9.53841 1.31946 9.53841 1.95262 9.92893 2.34315L15.5858 8L9.92893 13.6569C9.53841 14.0474 9.53841 14.6805 9.92893 15.0711C10.3195 15.4616 10.9526 15.4616 11.3431 15.0711L17.7071 8.70711ZM1 9H17V7H1V9Z" />
+          </svg>
+        </button>
+      </Link>
+    </StyledWrapper>
+  );
+};
+
+const StyledWrapper = styled.div`
+  .animated-button {
+    position: relative;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    padding: 16px 36px;
+    border: 4px solid;
+    border-color: transparent;
+    font-size: 16px;
+    background-color: inherit;
+    border-radius: 100px;
+    font-weight: 600;
+    color: white;
+    box-shadow: 0 0 0 2px white;
+    cursor: pointer;
+    overflow: hidden;
+    transition: all 0.6s cubic-bezier(0.23, 1, 0.32, 1);
+  }
+
+  .animated-button svg {
+    position: absolute;
+    width: 24px;
+    fill: white;
+    z-index: 9;
+    transition: all 0.8s cubic-bezier(0.23, 1, 0.32, 1);
+  }
+
+  .animated-button .arr-1 {
+    right: 16px;
+  }
+
+  .animated-button .arr-2 {
+    left: -25%;
+  }
+
+  .animated-button .circle {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 20px;
+    height: 20px;
+    background-color: white;
+    border-radius: 50%;
+    opacity: 0;
+    transition: all 0.8s cubic-bezier(0.23, 1, 0.32, 1);
+  }
+
+  .animated-button .text {
+    position: relative;
+    z-index: 1;
+    transform: translateX(-12px);
+    transition: all 0.8s cubic-bezier(0.23, 1, 0.32, 1);
+  }
+
+  .animated-button:hover {
+    box-shadow: 0 0 0 12px transparent;
+    color: black;
+  }
+
+  .animated-button:hover .arr-1 {
+    right: -25%;
+  }
+
+  .animated-button:hover .arr-2 {
+    left: 16px;
+  }
+
+  .animated-button:hover .text {
+    transform: translateX(12px);
+  }
+
+  .animated-button:hover svg {
+    fill: #000;
+  }
+
+  .animated-button:active {
+    scale: 0.95;
+    box-shadow: 0 0 0 4px white;
+  }
+
+  .animated-button:hover .circle {
+    width: 220px;
+    height: 220px;
+    opacity: 1;
+  }
+
+  @media (max-width: 640px) {
+    .animated-button {
+      font-size: 12px;
+      padding: 12px 36px;
+    }
+  }
+`;
---
+++ src/shared/ui/Cubs.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export const HomeCubs = () => {
+  return (
+    <>
+      <div className="header-cubs absolute left-0 top-0">
+        <svg
+          width="206"
+          height="297"
+          viewBox="0 0 206 297"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect x="99" width="107" height="100" fill="#272727" />
+          <rect y="100" width="99" height="100" fill="#202021" />
+          <rect x="99" y="200" width="107" height="97" fill="#131313" />
+        </svg>
+      </div>
+      <div className="header-cubs absolute right-0 top-0">
+        <svg
+          width="206"
+          height="297"
+          viewBox="0 0 206 297"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="107" height="100" fill="#272727" />
+          <rect x="107" y="100" width="99" height="100" fill="#202021" />
+          <rect y="200" width="107" height="97" fill="#131313" />
+        </svg>
+      </div>
+    </>
+  );
+};
+
+export const HomeAboutCubs = () => {
+  return (
+    <>
+      <div className="about-cubs absolute right-0 top-0">
+        <svg
+          viewBox="0 0 206 200"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect x="99" width="107" height="100" fill="#272727" />
+          <rect y="100" width="99" height="100" fill="#202021" />
+        </svg>
+      </div>
+    </>
+  );
+};
